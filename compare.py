d994bff671afbf4beb693ba3ceee3e26ded30382
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-18 14:34:54 -0500

    Comparison Class moved
    Comparison class moved to gameboard, combined various methods, removed unnecessary variables, refactored code to make multiplayer function correctly.

--- compare.py
+++ compare.py
@@ -1,81 +1,97 @@
 from player import Player
-from gameboard import Gameboards
+from ai import AI
+# player1 = ''
+# player2 = ''
+# player1_score = 0
+# player2_score = 0
 
 
-class Compare():
-    def __init__(self):
-        self.compare = ''
-    
-    def comparison( player1_selection, player2_selection):
-        
-        
-        
-        if player1_selection.upper == 'ROCK':
-            if player2_selection.upper == 'SCISSORS':
-                print (f'{player1_selection.name} wins because rock crushes scissors')
-                player1_selection.score += 1
-            elif player2_selection.upper == 'PAPER':
-                print (f'{player2.name} wins because paper covers rock.')
-                player2.score += 1
-            elif player2_selection.upper == 'SPOCK':
-                print (f'{player2.name} wins because Spock vaporizes rock.')
-                player2.score += 1
-            elif player2_selection.upper == 'LIZARD':
-                print (f'{player2.name} wins because lizard poisons Spock.')
-                player2.score += 1
-        
-        if player1_selection.upper == 'PAPER':
-            if player2_selection.upper == 'SCISSORS':
-                print (f'{player2.name} wins because scissors cut paper.')
-                player2.score += 1
-            elif player2_selection.upper == 'ROCK':
-                print (f'{player1_selection.name} wins because paper covers rock.')
-                player1.score += 1
-            elif player2.upper == 'SPOCK':
-                print (f'{player1_selection.name} wins because paper disproves Spock.')
-                player1.score += 1
-            elif player2_selection.upper == 'LIZARD':
-                print (f'{player2.name} wins because lizard eats paper.')
-                player2.score += 1
-        
-        if player1_selection.upper == 'SCISSORS':
-            if player2_selection.upper == 'PAPER':
-                print (f'{player1_selection.name} wins because scissors cut paper.')
-                player1.score +=1
-            if player2.upper == 'ROCK':
-                print(f'{player2.name} wins because rock crushes scissors.')
-                player2.score +=1
-            if player2_selection.upper == 'LIZARD':
-                print (f'{player1_selection.name} wins because scissors decapitate lizard.')
-                player1.score += 1
-            if player2_selection.upper == 'SPOCK':
-                print (f'{player2_selection.name} wins because Spock smashes scissors.')
-                player2.score += 1
 
-        if player1.upper == 'LIZARD':
-            if player2.upper == 'ROCK':
-                print(f'{player2.name} wins because Rock crushes Lizard')
-                player2.score += 1
-            elif player2.upper == 'PAPER':
-                print(f'{player1.name} wins because Lizard eats Paper')
-                player1.score += 1
-            elif player2.upper == 'SCISSORS':
-                print(f'{player2.name} wins because Scissors decapitates Lizard')
-                player2.score += 1
-            elif player2.upper == 'SPOCK':
-                print(f'{player2.name} wins because Lizard poisons Spock')
-                player2.score += 1
-        if player1.upper == 'SPOCK':
-            if player2.upper == 'ROCK':
-                print(f'{player1.name} wins because Spock vaporized Rock')
-                player1.score += 1
-            elif player2.upper == 'PAPER':
-                print(f'{player2.name} wins because Paper disproves Spock')
-                player2.score += 1
-            elif player2.upper == 'SCISSORS':
-                print(f'{player1.name} wins because Spock smashes Scissors')
-                player1.score += 1
-            elif player2.upper == 'LIZARD':
-                print(f'{player2.name} wins because Lizard poisons Spock')
-                player2.score += 1
-   
+# class Compare():
+#     def __init__(self):
+#         self.compare = ''
+        
+#     def comparison( player1_selection, player2_selection, player1, player2):
+#         player1_score = 0
+#         player2_score = 0
+        
+#         # while player1_score < (2) and player2_score < (2):
+            
+        
+#         if player1_selection.upper()  == ('ROCK'):
+#             if player2_selection.upper() == ('SCISSORS'):
+#                 print (f'{player1.name} wins because rock crushes scissors')
+#                 player1_score += 1
+#             elif player2_selection.upper() == ('PAPER'):
+#                 print (f'{player2.name} wins because paper covers rock.')
+#                 player2_score += 1
+                
+                
+
+#             elif player2_selection.upper() == ('SPOCK'):
+#                 print (f'{player2.name} wins because Spock vaporizes rock.')
+#                 player2_score += 1
+#             else:
+#                     #player2_selection.upper == 'LIZARD':
+#                 print (f'{player2.name} wins because lizard poisons Spock.')
+#                 player2_score += 1
+
+#         if player1_selection.upper() == ('PAPER'):
+#             if player2_selection.upper == ('SCISSORS'):
+#                 print (f'{player2.name} wins because scissors cut paper.')
+#                 player2_score += 1
+#             elif player2_selection.upper() == ('ROCK'):
+#                 print (f'{player1.name} wins because paper covers rock.')
+#                 player1_score += 1
+#             elif player2.upper() == ('SPOCK'):
+#                 print (f'{player1.name} wins because paper disproves Spock.')
+#                 player1_score += 1
+#             else:
+#                 # player2_selection.upper() == 'LIZARD':
+#                 print (f'{player2.name} wins because lizard eats paper.')
+#                 player2_score += 1
+
+#         if player1_selection.upper() == ('SCISSORS'):
+
+#             if player2_selection.upper() == ('PAPER'):
+#                 print (f'{player1.name} wins because scissors cut paper.')
+#                 player1_score +=1
+#             elif player2.upper() == ('ROCK'):
+#                 print(f'{player2.name} wins because rock crushes scissors.')
+#                 player2_score +=1
+#             elif player2_selection.upper() == ('LIZARD'):
+#                 print (f'{player1.name} wins because scissors decapitate lizard.')
+#                 player1_score += 1
+#             else:
+#                 # player2_selection.upper() == 'SPOCK':
+#                 print (f'{player2.name} wins because Spock smashes scissors.')
+#                 player2_score += 1
+
+#         if player1_selection.upper() == ('LIZARD'):
+#             if player2_selection.upper() == ('ROCK'):
+#                 print(f'{player2.name} wins because Rock crushes Lizard')
+#                 player2_score += 1
+#             elif player2_selection.upper() == ('PAPER'):
+#                 print(f'{player1.name} wins because Lizard eats Paper')
+#                 player1_score += 1
+#             elif player2_selection.upper() == ('SCISSORS'):
+#                 print(f'{player2.name} wins because Scissors decapitates Lizard')
+#                 player2_score += 1
+#             else:
+#                 # player2.upper() == 'SPOCK':
+#                 print(f'{player2.name} wins because Lizard poisons Spock')
+#                 player2_score += 1
+#         if player1_selection.upper() == ('SPOCK'):
+#             if player2_selection.upper() == ('ROCK'):
+#                 print(f'{player1.name} wins because Spock vaporized Rock')
+#                 player1_score += 1
+#             elif player2_selection.upper() == ('PAPER'):
+#                 print(f'{player2.name} wins because Paper disproves Spock')
+#                 player2_score += 1
+#             elif player2_selection.upper() == ('SCISSORS'):
+#                 print(f'{player1.name} wins because Spock smashes Scissors')
+#                 player1_score += 1
+#             else:
+#                 # player2.upper() == 'LIZARD':
+#                 print(f'{player2.name} wins because Lizard poisons Spock')
+#                 player2_score += 1
