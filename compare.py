734dfeeff5153e3397ce6855e7fc913cf9469238
unknown <nolan.beihl@gmail.com>
2021-10-18 09:07:03 -0600

    Update to files, added new class Compare

---
+++ compare.py
@@ -0,0 +1,7 @@
+class compare():
+    def __init__(self):
+        self.compare = ''
+    
+    def rock(self, player1, player2):
+        if player1.upper == 'ROCK' and player2.upper == 'SCISSORS':
+            
